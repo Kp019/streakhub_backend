1caf1acdc3889bd6e9f3a84598fff1a8908a6435
Joel J George <joeljoby111@gmail.com>
2023-11-04 23:56:36 +0530

    fix: solved the goal acheived isssue

--- index.js
+++ index.js
@@ -7,9 +7,22 @@ const supabaseurl = process.env.PUBLIC_SUPABASE_URL;
 const supabasekey = process.env.PUBLIC_SUPABASE_KEY;
 const supabase = createClient(supabaseurl, supabasekey);
 
+const { WakaTimeClient } = require("wakatime-client");
+const { RANGE } = require("wakatime-client");
+
 function addDays(date, days) {
   date.setDate(date.getDate() + days);
   return date;
+}
+function secondsToHms(d) {
+  d = Number(d);
+  var h = Math.floor(d / 3600);
+  var m = Math.floor((d % 3600) / 60);
+  var s = Math.floor((d % 3600) % 60);
+  var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+  var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+  return hDisplay + mDisplay + sDisplay;
 }
 
 async function getContributions(token, username) {
@@ -91,6 +104,25 @@ app.get("/maketodo/:id/:maker/:streak", async (req, res) => {
   res.json("hey");
 });
 
+app.get("/markthetodo/:id", async (req, res) => {
+  let id = Number(req.params.id);
+  let date = new Date();
+  date = date.toISOString();
+  const { data, error } = await supabase
+    .from("todo")
+    .update({ modified: date })
+    .eq("id", id);
+  res.json("hey");
+});
+
+// app.post("/fillthemall", (req, res) => {
+//   const {} = req.body;
+// });
+
+// app.post("/register", (req, res) => {
+//   const { name, username, password } = req.body;
+// });
+
 app.get("/connect/:id", async (req, res) => {
   let id = req.params;
   id = Number(id.id);
@@ -99,28 +131,35 @@ app.get("/connect/:id", async (req, res) => {
     .select("*")
     .eq("uid", id);
 
-  //   let todaysDate = new Date();
-  //   let date = new Date(main[0].github_last_accessed);
-  //   date.setHours(0, 0, 0, 0);
-  //   let yesterday = addDays(todaysDate, -1);
-  //   yesterday.setHours(0, 0, 0, 0);
-  //   if (String(yesterday) == String(date)) {
-  //     todaysDate.setHours(0, 0, 30, 0);
-  //     //check if today done by github
-  //     if (1) {
-  //       let ghstreak = main[0].github_streak;
-  //       ghstreak++;
-  //       let { data, error } = await supabase
-  //         .from("main")
-  //         .update({
-  //           github_streak: ghstreak,
-  //           github_last_accessed: todaysDate.,
-  //         })
-  //         .eq("uid", id);
-  //     }
-  //   }
-  //   res.json(main);
-  // });
+  const wakaclient = new WakaTimeClient(process.env.WAKATIME_TOKEN);
+  // const stats = await wakaclient.getMyStats({ range: RANGE.LAST_7_DAYS });
+
+  // let wakatime = stats.data;
+  let wakatime = await wakaclient.getDurations({
+    userId: "joel_j_george",
+    date: "2023-11-04",
+  });
+  wakatime = wakatime.data;
+  let duration = 0;
+  for (let i = 0; i < wakatime.length; i++) {
+    duration += wakatime[i].duration;
+  }
+  let durationstring = secondsToHms(duration);
+  console.log(durationstring);
+  let hrs = durationstring.split(" ")[0];
+  const hrstoreach = main[0].wakatime_goal;
+  if (Number(hrs) > hrstoreach) {
+    console.log("goal acheived hours more than goal");
+    const { data, error } = await supabase
+      .from("main")
+      .update({ wakatime_goal_achieved: 1 })
+      .eq("uid", id);
+  } else {
+    const { data, error } = await supabase
+      .from("main")
+      .update({ wakatime_goal_achieved: false })
+      .eq("uid", id);
+  }
 
   countContributionStreak(main[0].github_username).then((data) => {
     console.log(data);
@@ -136,6 +175,6 @@ app.get("/connect/:id", async (req, res) => {
       });
   });
 
-  res.json(main);
+  res.json(wakatime);
 });
 app.listen(4000);
